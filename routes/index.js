cc18fab513069d85693c6584efefefc7f731d61a
unknown <vishal.hari24@gmail.com>
2019-01-26 23:23:46 +0530

    Final Draft

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-//const csv = require('csvtojson');
 const csv = require('fast-csv');
 var async = require('asyncawait/async');
 var await = require('asyncawait/await');
@@ -7,97 +6,62 @@ var await = require('asyncawait/await');
 const router = express.Router();
 
 let Data = require('../models/Data')
+
 router.get('/', (req,res) => res.render('index'));
+
 router.get('/search', (req,res) => res.render('search', {msg:''}));
 
+router.get('/graph_search', (req,res) => res.render('graph_search', {msg:''}));
+
 router.get('/allstocks', (req,res) => {
-    var symbs = [];
-    /*Data.find().distinct('symbol', function(err, sym) {
-        //console.log(sym);
-        //symbs.push(sym);
-    });*/
-
-    /*(async (function(reqt, rest) {
-      var user = [];
-      var proj = {
-          _id: false
-      };
-      await (Data.find({symbol:'A'}, proj,function(err,pro){
-          user.push(pro);
-      }));
-      //console.log(user); //it's define
-        res.send(user);
-    }))();*/
-
-
-
-    (async (function(reqt, rest) {
-      var user = [];
-      var proj = {
-          _id: false
-      };
-      await (Data.find().distinct('symbol',function(err,pro){
-          //user.push(pro);
+    (async ((reqt, rest) => {
+      await (Data.find().distinct('symbol',(err,pro) => {
           res.render('allstocks', {
               stocks: pro
           })
       }));
-      //console.log(user); //it's define
-        //res.send(user);
     }))();
-
-
-    //console.log(symbs);
-
-
 });
 
 router.get('/stock/:sym', (req,res) => {
     var name = req.params.sym;
-    (async (function(reqt, rest) {
+    (async ((reqt, rest) => {
       var user = [];
       var proj = {
           _id: false,
           symbol: false
       };
-      await (Data.find({symbol: name}, proj, function(err,pro){
-          //user.push(pro);
+      await (Data.find({symbol: name}, proj, (err,pro) => {
           res.render('stock', {
               stock: pro,
               name: name
           })
       }));
-      //console.log(user); //it's define
-        //res.send(user);
     }))();
 });
 
 router.get('/graph/:sym', (req,res) => {
     var name = req.params.sym;
-    (async (function(reqt, rest) {
+    (async ((reqt, rest) => {
       var user = [];
       var proj = {
           _id: false,
           symbol: false,
       };
-      await (Data.find({symbol: name}, proj, function(err,pro){
-          //user.push(pro);
-
+      await (Data.find({symbol: name}, proj, (err,pro) => {
           res.render('graph', {
               stock: pro,
               name: name
           })
       }));
-      //console.log(user); //it's define
-        //res.send(user);
     }))();
 });
 
 router.get('/dfind', (req,res) => {
     var st = (req.query.stock).toUpperCase();
-    (async (function(reqt, rest) {
+    (async ((reqt, rest) => {
 
-      await (Data.count({symbol: st}, function(err,pro){
+      await (Data.count({symbol: st}, (err,pro) => {
          if(pro != 0) {
              res.redirect('/stock/' + st);
          } else {
@@ -111,9 +75,9 @@ router.get('/dfind', (req,res) => {
 
 router.get('/gfind', (req,res) => {
     var st = (req.query.stock).toUpperCase();
-    (async (function(reqt, rest) {
+    (async ((reqt, rest) => {
 
-      await (Data.count({symbol: st}, function(err,pro){
+      await (Data.count({symbol: st}, (err,pro) => {
          if(pro != 0) {
              res.redirect('/graph/' + st);
          } else {
@@ -125,29 +89,8 @@ router.get('/gfind', (req,res) => {
     }))();
 });
 
-router.get('/verify', (req,res) => {
-    var cnt = 0;
-    function getDistinct(name) {
-        var query = Data.find().distinct('symbol').exec();
-        return query;
-    }
-    var query = getDistinct('hello');
-    query.then(function (sym) {
-        sym.forEach(function (s) {
-            //console.log(s);
-            stockList.push(s);
-        })
-    });
-    setTimeout(() => res.send(stockList + stockList.length),5000);
-    console.log(stockList);
-    /*Data.find({symbol:'WLTW'}, function(err, stock) {
-        if(err) {
-            console.log(err);
-        } else {
-            console.log(stock);
-        }
-    });*/
+router.get('/about', (req,res) => {
+    res.render('profile');
 });
-
 
 module.exports = router;
